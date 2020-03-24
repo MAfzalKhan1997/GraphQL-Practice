775b2d16d7c1fbe6993745fbea63ffa2e0045e84
Muhammad Afzal Khan <mafzalkhan1997@gmail.com>
2020-03-24 22:40:15 +0500

    done till yet

--- schema/schema.js
+++ schema/schema.js
@@ -83,6 +83,31 @@ const mutation = new GraphQLObjectType({
           .post(`http://localhost:3000/users`, { firstName, age })
           .then(res => res.data);
       }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .delete(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
+      }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then(res => res.data);
+      }
     }
   }
 });
